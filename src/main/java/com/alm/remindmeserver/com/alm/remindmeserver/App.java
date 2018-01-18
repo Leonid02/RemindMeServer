28576c6005412fc5dc9c04f165925990d716b599
leon <lyanovs@gmail.com>
2018-01-18 18:09:07 +0200

    Initial Commit Remind Me Server project

---
+++ src/main/java/com/alm/remindmeserver/com/alm/remindmeserver/App.java
@@ -0,0 +1,13 @@
+package com.alm.remindmeserver.com.alm.remindmeserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
